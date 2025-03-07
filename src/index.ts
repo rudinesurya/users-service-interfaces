5b27749490037037cb559012cca1d64945701820
rudinesurya <rudinesurya@gmail.com>
2025-03-07 16:32:39 +0000

    update

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './user.interface';
-export * from './user-create-response.interface';
-export * from './user-search-response.interface';
-export * from './user-update-response.interface';
-export * from './user-update.interface';
